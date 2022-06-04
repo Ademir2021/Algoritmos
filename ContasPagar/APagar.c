ce66680095af970393ed0da1d88ebd95b7838477
Ademir Souza de Almeida <centroserra@gmail.com>
2022-06-04 09:21:48 -0300

    ajustes gerais

--- ContasPagar/APagar.c
+++ ContasPagar/APagar.c
@@ -1,22 +1,81 @@
 #include<stdio.h>
+#include<locale.h>
 
-int main(){
-    int i, NumContas;
-    float soma, valores[50];
+#define tam 8
 
-    printf("Digite o numero de contas a pagar: ");
-    scanf("%d", &NumContas);
+//variaveis Globais
+int i;
+int NumContas;
+float soma, valores[tam];
+char contas[tam][200]={
+    "Àgua",
+    "Luz",
+    "Internet",
+    "Das Mei",
+    "Evolusoft",
+    "Prest. Celular",
+    "Outros"
+    };
+float previsto[tam]={
+    89.00,
+    150.00,
+    55.00,
+    65.00,
+    32.00,
+    328.00,
+    20.00
+};
 
-    for(i=1; i<=NumContas; i++){
-        printf("Digite o valor da %dª conta: ", i);
-        scanf("%f", &valores[i]);
+//Prototipação
+void contasPagar();
+void pagarContas();
+void contasPrevista();
+void contasPagas();
+
+int main() {
+    setlocale(LC_ALL, "Portuguese");
+    
+    contasPagar();
+    pagarContas();
+    contasPrevista();
+    contasPagas();
+
+return (0);
+getchar();
 }
 
-    for (i=1; i<=NumContas; i++)
-    soma = soma + valores[i];
+void contasPagar() {
+   printf("\nContas a pagar por ordem de Prioridades:");
+   printf("\n[ ");
+   for (i = 0; i<tam; i++) {
+       printf("| %s ", contas[i]);
+   }
+   printf("| ]\n\n");
+}
 
-    printf("Total a pagar é: R$%.3f\n", soma);
+void pagarContas() {
+    printf("Digite o numero de contas a pagar: ");
+    scanf("%d", &NumContas);
+    for(i=0; i<NumContas; i++) {
+        printf("\nDigite o valor da %dª conta de %s: ", i+1, contas[i]);
+        scanf("%f", &valores[i]);
+        printf("Conta de %s Paga: %3f \n", contas[i], valores[i]);
+    }
+}
 
-return 0;
+void contasPrevista() {
+    printf("\nContas Previstas:");
+    for(i=0; i<valores[i]; i++) {
+        printf("\n%s: R$%3f",contas[i], previsto[i]);
+    }
+}
 
+void contasPagas() {
+    printf("\n\nContas Pagas:");
+    for(i=0; i<valores[i]; i++) {
+        printf("\n%s: R$%3f",contas[i], valores[i]);
+    }
+    for (i=0; i<NumContas; i++)
+    soma += + valores[i];
+    printf("\nTotal de pagamentos: R$%.3f\n\n", soma);
 }
---
+++ conectivosLogicos.md
@@ -0,0 +1,11 @@
+# Conectivos lógicos
+
+- Disjunção || A disjunção entre duas variáveis resulta em um valor verdadeiro quando pelo menos uma das variáveis é verdadeira.
+Ou seja quando pelo menos uma das "variáveis" satisfaça uma condição
+
+- Conjunção && A conjunção entre duas variáveis resulta em um valor somente quando as duas "variáveis" são verdadeiras.
+Ou seja quando as duas "variáveis" satisfaça uma condição.
+
+- Resumo
+- && Conjunção (E lógico)
+- || Disjunção (OU lógico)
